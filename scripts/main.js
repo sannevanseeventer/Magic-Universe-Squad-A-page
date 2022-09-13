54adb892a720f1679343ec9505189d2e78c9e0c8
Robin <robin.django@gmail.com>
2022-09-13 15:23:26 +0200

    Comments voor overzicht & wat formatting

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,18 @@
 // Globale functie om de code te laten wachten (argument is in milliseconden)
 function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-// Functie voor het omdraaien van alle kaarten
+// Functie voor het omdraaien van alle kaarten (wordt geroepen door de knop)
 async function turnCards() {
-    // Een voor een alle kaarten langs om ze te laten draaien    
-    for (let i = 1; i <= 25; i++) {
-        let card = document.getElementById(i);
-        await delay(100);
-        card.classList.toggle("turned");
-        card.classList.toggle("face-up");
-    }
+  // Eén voor één alle kaarten langs om ze te laten draaien
+  for (let i = 1; i <= 25; i++) {
+    let card = document.getElementById(i);
+
+    card.classList.toggle("turned");
+    card.classList.toggle("face-up");
+    
+    // Wacht 100 milliseconden voordat het doorgaat naar de volgende kaart
+    await delay(100);
+  }
 }
