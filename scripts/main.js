f4e990e38b59349c1eb9afc1cb1303c2bfd0e3b7
Robin <robin.django@gmail.com>
2022-09-13 15:12:49 +0200

    Aantal namen Engels gemaakt. Animatie veranderd

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,15 @@
-
+// Globale functie om de code te laten wachten (argument is in milliseconden)
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
-async function omdraaien() {
-    // const kaarten = document.querySelector(".card");
-    
-    // console.log(kaarten);
-    // kaarten.classList.toggle("omgedraaid");
-        
-    for (let i = 1; i <= 25; i++){
-        console.log (i);
+// Functie voor het omdraaien van alle kaarten
+async function turnCards() {
+    // Een voor een alle kaarten langs om ze te laten draaien    
+    for (let i = 1; i <= 25; i++) {
         let card = document.getElementById(i);
         await delay(100);
-        card.classList.toggle("omgedraaid");
+        card.classList.toggle("turned");
+        card.classList.toggle("face-up");
     }
 }
