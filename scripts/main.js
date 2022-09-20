1d19cd92bc04aee479f6a0b0cf0e18ed4819f5da
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2022-09-20 13:15:18 +0200

    darkmode

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,21 @@
 function delay(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
-
+ 
 // Functie voor het omdraaien van alle kaarten (wordt geroepen door de knop)
 async function turnCards() {
   // Eén voor één alle kaarten langs om ze te laten draaien
-  for (let i = 1; i <= 22; i++) {
+  let button = document.getElementById("turn-button");
+  let fill = document.getElementById("toggle-fill");
+
+  button.classList.toggle("on")
+  fill.classList.toggle("toggled");
+
+  for (let i = 1; i <= 25; i++) {
     let card = document.getElementById(i);
 
-    card.classList.toggle("turned");
-    card.classList.toggle("face-up");
+    card.classList.toggle("card-front");
+    card.classList.toggle("card-back");
     
     // Wacht 100 milliseconden voordat het doorgaat naar de volgende kaart
     await delay(100);
