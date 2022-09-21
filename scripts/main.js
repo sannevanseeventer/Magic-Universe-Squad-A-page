c1622993dfcc485f3d975cfdd42af24c355e6080
Robin <robin.django@gmail.com>
2022-09-21 13:19:33 +0200

    Kaarten selecteren
    Robin & Fatima

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,31 @@ function delay(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
  
+const addEventListeners = () => {
+  const cards = document.querySelectorAll(".card");
+  const cardsArr = Array.prototype.slice.call(cards);
+
+  cardsArr.forEach(
+    (card) => {
+      card.addEventListener("click", function(){selectCard(card)})
+  });
+}
+
+
 // Functie voor het omdraaien van alle kaarten (wordt geroepen door de knop)
 async function turnCards() {
   // Eén voor één alle kaarten langs om ze te laten draaien
   const button = document.getElementById("turn-button");
   const fill = document.getElementById("toggle-fill");
+
+  let selectedCards = document.querySelectorAll(".selected");
+  let selectedCardsArr = Array.prototype.slice.call(selectedCards);
+
+  if (selectedCardsArr.length > 0) {
+    selectedCardsArr.forEach((card) => {
+      card.classList.toggle("selected");
+    });
+  }
   
   button.classList.toggle("on")
   fill.classList.toggle("toggled");
@@ -21,4 +41,18 @@ async function turnCards() {
     // Wacht 100 milliseconden voordat het doorgaat naar de volgende kaart
     await delay(100);
   };
+}
+
+const selectCard = (card) => {
+  console.log(card)
+  let selectedCards = document.querySelectorAll(".selected");
+  let selectedCardsArr = Array.prototype.slice.call(selectedCards);
+
+  if (selectedCardsArr.length > 0) {
+    selectedCardsArr.forEach((card) => {
+      card.classList.toggle("selected");
+    });
+  }
+
+  card.classList.toggle("selected");
 }
